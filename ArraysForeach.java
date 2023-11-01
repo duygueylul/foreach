c807c3e80376fbdeea855fa3afaf9a0cb9ef5490
Duygu Eylül KARATAŞ <96980093+duygueylul@users.noreply.github.com>
2023-11-02 01:25:50 +0300

    Initial commit

---
+++ ArraysForeach.java
@@ -0,0 +1,14 @@
+package JavaArrays;
+
+public class ArraysForeach {
+
+	public static void main(String[] args) {
+		 int [] sayilar = {100,200,300,400,500,600,700,800,900,1000};
+		 
+		 for (int sayi : sayilar) { //foreach kullanımı
+	            System.out.print(sayi+" ");
+	        }
+
+	}
+
+}
